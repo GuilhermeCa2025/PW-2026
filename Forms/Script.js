e0d30f86a8cceaf5c01109a594436a3edd65cad1
GuilhermeCa2025 <gr8020744@gmail.com>
2026-05-12 16:09:31 -0300

    Aula 12-05

---
+++ Forms/Script.js
@@ -0,0 +1,95 @@
+const modal = document.querySelector('.modal-container')
+const tbody = document.querySelector('tbody')
+const sNome = document.querySelector('#m-nome')
+const sFuncao = document.querySelector('#m-função') // Note o seletor igual ao HTML
+const sSalario = document.querySelector('#m-salário')
+const btnSalvar = document.querySelector('#btn-salvar')
+
+let itens
+let id
+
+// Adicionei o listener para o botão de "Incluir" que estava faltando no HTML
+document.querySelector('#new').onclick = () => openModal()
+
+function openModal(edit = false, index = 0) {
+    modal.classList.add('active')
+
+    modal.onclick = e => {
+        // Corrigido: className com 'c' minúsculo
+        if (e.target.className.indexOf('modal-container') !== -1){
+            modal.classList.remove('active')
+        }
+    }
+
+    if (edit) {
+        sNome.value = itens[index].nome
+        sFuncao.value = itens[index].funcao
+        sSalario.value = itens[index].salario
+        id = index
+    } else {
+        sNome.value = ''
+        sFuncao.value = ''
+        sSalario.value = ''
+    }
+}
+
+function editItem(index){
+    openModal(true, index)
+}
+
+function deleteItem(index){
+    itens.splice(index, 1)
+    setItensBD()
+    loadItens()
+}
+
+function insertItem(item, index) {
+    let tr = document.createElement('tr')
+    tr.innerHTML = `
+        <td>${item.nome}</td>
+        <td>${item.funcao}</td>
+        <td>R$ ${item.salario}</td>
+        <td class="acao">
+            <button onclick="editItem(${index})"><i class='bx bx-edit'></i></button>
+        </td>
+        <td class="acao">
+            <button onclick="deleteItem(${index})"><i class='bx bx-trash'></i></button>
+        </td>
+    `
+    tbody.appendChild(tr)
+}
+
+btnSalvar.onclick = e => { 
+    if (sNome.value == '' || sFuncao.value == '' || sSalario.value == '') { 
+        return 
+    }
+
+    e.preventDefault();
+
+    if (id !== undefined) { 
+        itens[id].nome = sNome.value 
+        itens[id].funcao = sFuncao.value 
+        itens[id].salario = sSalario.value 
+    } else { 
+        itens.push({'nome': sNome.value, 'funcao': sFuncao.value, 'salario': sSalario.value}) 
+    }
+
+    setItensBD()
+    modal.classList.remove('active') 
+    loadItens() 
+    id = undefined 
+}
+
+// Movido para fora para ser acessível globalmente
+function loadItens() { 
+    itens = getItensBD()    
+    tbody.innerHTML = '' 
+    itens.forEach((item, index) => { 
+        insertItem(item, index) 
+    })  
+}
+
+const getItensBD = () => JSON.parse(localStorage.getItem('dbfunc')) ?? [] 
+const setItensBD = () => localStorage.setItem('dbfunc', JSON.stringify(itens))
+
+loadItens()
